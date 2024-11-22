cbdddab98cbf338872da16e0c0374910396b0edc
LathikaMBH <lathika.mbh@gmail.com>
2024-11-23 03:07:23 +0530

    added webpage title verify assertion and added headless attribute to playwright.config file

--- tests/MyfirstPlaywrighttest.spec.ts
+++ tests/MyfirstPlaywrighttest.spec.ts
@@ -1,4 +1,6 @@
-const {test} = require('@playwright/test');
+import { expect } from "@playwright/test";
+
+const { test } = require('@playwright/test');
 
 test('Browser Context Playwright test', async({browser})=>
 {
@@ -8,7 +10,11 @@ test('Browser Context Playwright test', async({browser})=>
 
 });
 
-test('Page Playwright test', async({page})=>
+test.only('Page Playwright test', async({page})=>
 {
     await page.goto("https://playwright.dev/");
+    //get the title -> assert it
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+
 });

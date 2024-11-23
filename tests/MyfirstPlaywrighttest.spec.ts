f68e4dcbe9b6e746689911aad41441e128cd290e
LathikaMBH <lathika.mbh@gmail.com>
2024-11-23 17:29:05 +0530

    added test case to demo how to select one element amoung multiple same elements

--- tests/MyfirstPlaywrighttest.spec.ts
+++ tests/MyfirstPlaywrighttest.spec.ts
@@ -1,6 +1,7 @@
-import { expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-const { test } = require('@playwright/test');
+
+
 
 test('Browser Context Playwright test', async({browser})=>
 {
@@ -24,15 +25,27 @@ test('Page Playwright test', async({page})=>
 
 //wrong username and password + validate error message + extract text + Containstext
 
-test.only('Browser Context Playwright test- validate error message', async({browser})=>
+test('Browser Context Playwright test- validate error message', async({browser})=>
 {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     await page.locator('[name="username"]').fill("Admin");
-    await page.locator('[name="password"]').type("admin1234"); 
+    await page.locator('[name="password"]').fill("admin1234"); 
     await page.locator('[type="submit"]').click();
     console.log(await page.locator('[role= "alert"]').textContent());
     await expect(page.locator('[role= "alert"]')).toContainText("Invalid credentials");
+
+});
+
+//extract text from specific element when there are multiple elements
+
+test.only('Select one element amoung multiple same elements', async({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator('[name="username"]').fill("rahulshettyacademy");
+    await page.locator('[name="password"]').fill("learning"); 
+    await page.locator('[type="submit"]').click();
+    console.log(await page.locator(".card-body a").nth(0).textContent());
 
 });
